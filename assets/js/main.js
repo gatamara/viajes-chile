8a1a21bad58614eed3fde0b8d2d56b4d8ec82218
Tamara Salinas <dev.tamara29@gmail.com>
2024-07-16 22:47:50 -0400

    agregando componenetes que utilicen javascript, como tooltips y  modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     var hash = this.hash;
     $('html, body').animate({
     scrollTop: $(hash).offset().top
-    }, 800, function(){
+    }, 400, function(){
    
     window.location.hash = hash;
     });
