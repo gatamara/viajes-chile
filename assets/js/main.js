5db07dba5d7d8fe94dab77268a0e1564f5860d35
Tamara Salinas <dev.tamara29@gmail.com>
2024-07-16 20:26:24 -0400

    smooth scroll y opacidad en el nav al hacer scroll

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+    //Smooth scroll con jquery - sacado de la presentacion dia 12
+    $(".nav-link").on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+    scrollTop: $(hash).offset().top
+    }, 800, function(){
+   
+    window.location.hash = hash;
+    });
+    }
+    });
+
+    //agregue una opacidad del hacer scroll, con el id nav 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) { 
+            $('#nav').addClass('scroll-nav');
+        } else {
+            $('#nav').removeClass('scroll-nav');
+        }
+    });
+
+
+});
+   
